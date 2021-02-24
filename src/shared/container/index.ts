f0964a1607116099186daa21d6d0df75d8bfcce8
Dario gondim <cognusdev@gmail.com>
2021-02-24 16:07:00 -0300

    add client has user in container tsringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
+import IClientsHasUsersRepository from '@modules/users_has_clients/repositories/IClientsHasUsersRepository';
+import ClientsHasUsersRepository from '@modules/users_has_clients/infra/typeorm/repositories/ClientsHasUsersRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -17,4 +20,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<IClientsHasUsersRepository>(
+  'ClientsHasUsersRepository',
+  ClientsHasUsersRepository,
 );
