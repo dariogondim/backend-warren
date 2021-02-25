dd1ff267197f961a183125697df22697ae86e93c
Dario gondim <cognusdev@gmail.com>
2021-02-25 00:06:12 -0300

    add create deposit dto

---
+++ src/modules/bankTransactions/dtos/ICreateDepositDTO.ts
@@ -0,0 +1,11 @@
+export default interface ICreateDepositDTO {
+  originTransaction: string;
+  channel: string;
+  channelDescription: string;
+  value: number;
+  typeTransaction: string;
+  bank_account_sender_id: string; // de onde parte o depósito
+  status: string; // gerado no service
+  compensationDate: Date; // gerado no service
+  profitability_id?: string; // pode ser nulo se nao tiver rentabilidade
+}
