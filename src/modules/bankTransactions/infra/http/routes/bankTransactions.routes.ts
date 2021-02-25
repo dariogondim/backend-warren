8d39ca08c90783f1ef222f39f55a01d81b249445
Dario gondim <cognusdev@gmail.com>
2021-02-25 14:56:33 -0300

    add authenticate bank transaction

--- src/modules/bankTransactions/infra/http/routes/bankTransactions.routes.ts
+++ src/modules/bankTransactions/infra/http/routes/bankTransactions.routes.ts
@@ -1,8 +1,11 @@
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import { Router } from 'express';
 import BankTransactionsController from '../controllers/BankTransactionsController';
 
 const bankTransactionsRouter = Router();
 const bankTransactionController = new BankTransactionsController();
+
+bankTransactionsRouter.use(ensureAuthenticated);
 
 bankTransactionsRouter.post(
   '/deposit',
