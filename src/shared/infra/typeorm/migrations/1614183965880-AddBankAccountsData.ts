7e369e394d51b659db120f80e53f8ff9e1d11ea9
Dario gondim <cognusdev@gmail.com>
2021-02-25 00:10:56 -0300

    change migration data

--- src/shared/infra/typeorm/migrations/1614183965880-AddBankAccountsData.ts
+++ src/shared/infra/typeorm/migrations/1614183965880-AddBankAccountsData.ts
@@ -9,6 +9,7 @@ export default class AddBankAccountsData1614183965880
       .into('bank_accounts')
       .values([
         {
+          id: 'a4b671be-a892-4288-807f-9241bf0c8645',
           cc: '11702761',
           agency_id: '948ee330-5c92-457b-bb9c-38fefe47a5e8',
           client_id: 'a1345985-627f-4129-80bf-2e08e5daccda',
@@ -16,6 +17,7 @@ export default class AddBankAccountsData1614183965880
           profitability_id: 'eb85f0f2-2526-4935-b005-78d048c599f7',
         },
         {
+          id: '50cde7c1-4e1e-44b1-aefc-671ce2b6c673',
           cc: '11812370',
           agency_id: '0df4edcb-db3e-43cb-894a-470095e912a5',
           client_id: 'b3fe0e30-7eee-41a4-9d8b-65c04afcd8d6',
@@ -23,6 +25,7 @@ export default class AddBankAccountsData1614183965880
           profitability_id: 'eb85f0f2-2526-4935-b005-78d048c599f7',
         },
         {
+          id: 'c8e5a7f0-18d5-43ff-a92d-563a48f5d33e',
           cc: '2228688',
           agency_id: 'a9f8660d-1516-40b0-886e-c4f4189d6793',
           client_id: 'a1345985-627f-4129-80bf-2e08e5daccda',
@@ -30,6 +33,7 @@ export default class AddBankAccountsData1614183965880
           profitability_id: null,
         },
         {
+          id: '1a2f0a9e-2167-4eb0-99fb-cc7c738dee97',
           cc: '10394516',
           agency_id: 'f978b2e4-fd61-4b8c-adaf-f5cbddc46102',
           client_id: 'ff977231-dafa-4c59-94dc-9b0089c8576c',
@@ -37,6 +41,7 @@ export default class AddBankAccountsData1614183965880
           profitability_id: 'eb85f0f2-2526-4935-b005-78d048c599f7',
         },
         {
+          id: 'f62edd8f-9edd-495a-b909-34a8fe03761c',
           cc: '10007210',
           agency_id: '24a08a94-1a19-4b97-b81c-0108c95b094c',
           client_id: 'fd536c6f-26fd-4986-afb2-5332c0b5ed67',
