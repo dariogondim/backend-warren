a07cfef857a09cf9a15c5a1663b82d7f01d7698d
Dario gondim <cognusdev@gmail.com>
2021-02-26 00:40:43 -0300

    add dto withdraw

---
+++ src/modules/bankTransactions/dtos/ICreateWithdrawDTO.ts
@@ -0,0 +1,9 @@
+export default interface ICreateWithdrawDTO {
+  originTransaction: string;
+  channel: string;
+  channelDescription: string;
+  value: number;
+  typeTransaction: string;
+  bank_account_sender_id: string; // de onde parte o saque
+  status: string; // gerado no service
+}
