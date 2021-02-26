0569a6af80d643967bede0ef6280cba9993ffc74
Dario gondim <cognusdev@gmail.com>
2021-02-26 00:41:53 -0300

    add withdraw and balance functions

--- src/modules/bankTransactions/infra/http/controllers/BankTransactionsController.ts
+++ src/modules/bankTransactions/infra/http/controllers/BankTransactionsController.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 
 import { Transactional } from 'typeorm-transactional-cls-hooked';
 import CreateDepositService from '@modules/bankTransactions/services/CreateDepositService';
+import CreateWithdrawService from '@modules/bankTransactions/services/CreateWithdrawService';
+import RetrieveBalanceService from '@modules/bankTransactions/services/RetrieveBalanceService';
 
 export default class BankTransactionsController {
   @Transactional()
@@ -29,6 +31,52 @@ export default class BankTransactionsController {
       value,
       bank_account_sender_id,
       user_id,
+    });
+
+    return response.json(bankTransaction);
+  }
+
+  public async createWithdraw(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const {
+      channel,
+      channelDescription,
+      value,
+      bank_account_sender_id,
+    } = request.body;
+
+    const { user_id } = request.body;
+
+    const createBankTransaction = container.resolve(CreateWithdrawService);
+
+    const bankTransaction = await createBankTransaction.execute({
+      channel,
+      channelDescription,
+      value,
+      bank_account_sender_id,
+      user_id,
+    });
+
+    return response.json(bankTransaction);
+  }
+
+  public async calculateBalanceService(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const { bank_account_sender_id, from, to } = request.body;
+
+    const { user_id } = request.body;
+
+    const createBankTransaction = container.resolve(RetrieveBalanceService);
+
+    const bankTransaction = await createBankTransaction.execute({
+      bank_account_sender_id,
+      user_id,
+      from,
+      to,
     });
 
     return response.json(bankTransaction);
