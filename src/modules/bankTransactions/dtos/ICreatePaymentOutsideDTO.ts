70245580ae63b80f9320ba359341538c17780d2d
Dario gondim <cognusdev@gmail.com>
2021-02-27 00:18:39 -0300

    changes in dto external payments

--- src/modules/bankTransactions/dtos/ICreatePaymentOutsideDTO.ts
+++ src/modules/bankTransactions/dtos/ICreatePaymentOutsideDTO.ts
@@ -8,9 +8,8 @@ export default interface ICreatePaymentOutsideDTO {
   status: string; // gerado no service
   compensationDate: Date; // gerado no service
   memo: string;
-  bank_account_recipient_id: string; // o alvo do pagamento, opcional se é pra outro banco
   bank_destiny_id: string; // o banco de destino do pagamento, obrigatório se for pra outro banco
+  agencyDestiny: string; // a agência de destino do pagamento,  obrigatório se for pra outro banco
   cpfDestiny: string; // o cpf de destino do pagamento, obrigatório se for pra outro banco
   accountDestiny: string; // a conta de destino do pagamento, obrigatório se for pra outro banco
-  profitability_id?: string; // pode ser nulo se nao tiver rentabilidade
 }
